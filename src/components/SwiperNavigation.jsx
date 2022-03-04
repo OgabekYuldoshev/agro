6f5aa0d07ea405cc47f8beef9990c06e9b9cd3a9
OgabekYuldoshev <yuldashoff1@gmail.com>
2022-03-04 14:45:39 +0500

    asdad

--- src/components/SwiperNavigation.jsx
+++ src/components/SwiperNavigation.jsx
@@ -8,13 +8,13 @@ import SwiperCore, {
   Autoplay
 } from 'swiper'
 
-// ** Images
-import img1 from '@src/assets/images/banner/banner-20.jpg'
-import img2 from '@src/assets/images/banner/banner-7.jpg'
-import img3 from '@src/assets/images/banner/banner-8.jpg'
-import img4 from '@src/assets/images/banner/banner-9.jpg'
-import img5 from '@src/assets/images/banner/banner-10.jpg'
-import img6 from '@src/assets/images/banner/banner-11.jpg'
+// // ** Images
+// import img1 from '@src/assets/images/banner/banner-20.jpg'
+// import img2 from '@src/assets/images/banner/banner-7.jpg'
+// import img3 from '@src/assets/images/banner/banner-8.jpg'
+// import img4 from '@src/assets/images/banner/banner-9.jpg'
+// import img5 from '@src/assets/images/banner/banner-10.jpg'
+// import img6 from '@src/assets/images/banner/banner-11.jpg'
 
 const params = {
   autoplay: {
@@ -33,29 +33,11 @@ const SwiperAutoplay = ({ sliders }) => {
     <Swiper className='mt-2' {...params}>
       {
         sliders?.map((slide, index) => (
-          <SwiperSlide key={index} className="d-flex align-items-center justify-content-center" style={{ height: "10vh" }}>
+          <SwiperSlide key={index} className="d-flex align-items-center justify-content-center" style={{ maxHeight: "400px", minHeight: "200px" }}>
             <img src={baseUrl + slide.image} alt={slide.name} id="imgUNcover" />
           </SwiperSlide>
         ))
       }
-      <SwiperSlide>
-        <img src={img1} alt='swiper 1' className='img-fluid' />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img2} alt='swiper 2' className='img-fluid' />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img3} alt='swiper 3' className='img-fluid' />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img4} alt='swiper 4' className='img-fluid' />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img5} alt='swiper 5' className='img-fluid' />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={img6} alt='swiper 6' className='img-fluid' />
-      </SwiperSlide>
     </Swiper>
   )
 }
